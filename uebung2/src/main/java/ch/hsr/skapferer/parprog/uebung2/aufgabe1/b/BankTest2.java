9e71c47adde13752f419a660b49a8460be7cfb3e
Stefan Kapferer <stefan@kapferer.ch>
2015-02-25 13:38:39 +0100

    Uebung2/Aufgabe1: Tested synchronized methods

---
+++ uebung2/src/main/java/ch/hsr/skapferer/parprog/uebung2/aufgabe1/b/BankTest2.java
@@ -0,0 +1,68 @@
+package ch.hsr.skapferer.parprog.uebung2.aufgabe1.b;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class BankAccount {
+	private int balance = 0;
+
+	public synchronized void deposit(int amount) {
+		balance += amount;
+	}
+
+	public synchronized boolean withdraw(int amount) {
+		if (amount <= balance) {
+			balance -= amount;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+}
+
+class BankCustomer extends Thread {
+	private static final int NOF_TRANSACTIONS = 10000000;
+	private final BankAccount account;
+
+	public BankCustomer(BankAccount account) {
+		this.account = account;
+	}
+
+	@Override
+	public void run() {
+		for (int k = 0; k < NOF_TRANSACTIONS; k++) {
+			account.deposit(100);
+			account.withdraw(100);
+		}
+	}
+}
+
+public class BankTest2 {
+	private static final int NOF_CUSTOMERS = 10;
+
+	public static void main(String[] args) {
+		BankAccount account = new BankAccount();
+		List<BankCustomer> customers = new ArrayList<>();
+		for (int i = 0; i < NOF_CUSTOMERS; i++) {
+			BankCustomer customer = new BankCustomer(account);
+			customers.add(customer);
+			customer.start();
+		}
+		joinCustomers(customers);
+		assert account.getBalance() == 0;
+	}
+
+	private static void joinCustomers(List<BankCustomer> customers) {
+		for (BankCustomer customer : customers) {
+			try {
+				customer.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
