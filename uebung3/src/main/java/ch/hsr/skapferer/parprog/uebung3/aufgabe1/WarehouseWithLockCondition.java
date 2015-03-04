6bdaeab3c8a9b61eeb7f3d9645c88d5405252b55
Stefan Kapferer <stefan@kapferer.ch>
2015-03-04 13:30:06 +0100

    Uebung3/Aufgabe1: Implemented WarehouseWithLockCondition

--- uebung3/src/main/java/ch/hsr/skapferer/parprog/uebung3/aufgabe1/WarehouseWithLockCondition.java
+++ uebung3/src/main/java/ch/hsr/skapferer/parprog/uebung3/aufgabe1/WarehouseWithLockCondition.java
@@ -1,19 +1,52 @@
 package ch.hsr.skapferer.parprog.uebung3.aufgabe1;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class WarehouseWithLockCondition implements Warehouse {
-	// TODO
+
+	private int capacity;
+	private int currentAmount = 0;
+	private boolean fair;
+
+	private Lock monitor;
+	private Condition nonFull;
+	private Condition nonEmpty;
 
 	public WarehouseWithLockCondition(int capacity, boolean fair) {
-		// TODO
+		this.capacity = capacity;
+		this.fair = fair;
+		this.monitor = new ReentrantLock(this.fair);
+		this.nonFull = this.monitor.newCondition();
+		this.nonEmpty = this.monitor.newCondition();
 	}
 
 	@Override
 	public void put(int amount) throws InterruptedException {
-		// TODO
+		monitor.lock();
+		try {
+			while ((currentAmount + amount) > capacity) {
+				nonFull.await();
+			}
+			currentAmount = currentAmount + amount;
+			nonEmpty.signal();
+		} finally {
+			monitor.unlock();
+		}
 	}
 
 	@Override
 	public void get(int amount) throws InterruptedException {
-		// TODO
+		monitor.lock();
+		try {
+			while (currentAmount < amount) {
+				nonEmpty.await();
+			}
+			currentAmount = currentAmount - amount;
+			nonFull.signal();
+		} finally {
+			monitor.unlock();
+		}
 	}
 }
