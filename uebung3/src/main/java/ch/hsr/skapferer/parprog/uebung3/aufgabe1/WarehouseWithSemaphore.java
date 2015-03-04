e94c1e132d263ccdafee558fdcdf61b78fc83d8f
Stefan Kapferer <stefan@kapferer.ch>
2015-03-04 13:09:32 +0100

    Uebung3/Aufgabe1: Implemented WarehouseWithSemaphore

--- uebung3/src/main/java/ch/hsr/skapferer/parprog/uebung3/aufgabe1/WarehouseWithSemaphore.java
+++ uebung3/src/main/java/ch/hsr/skapferer/parprog/uebung3/aufgabe1/WarehouseWithSemaphore.java
@@ -1,19 +1,37 @@
 package ch.hsr.skapferer.parprog.uebung3.aufgabe1;
 
+import java.util.concurrent.Semaphore;
+
 public class WarehouseWithSemaphore implements Warehouse {
-	// TODO
+
+	private int capacity;
+	private boolean fair;
+	private int currentAmount = 0;
+	private Semaphore upperLimit;
+	private Semaphore lowerLimit;
 
 	public WarehouseWithSemaphore(int capacity, boolean fair) {
-		// TODO
+		this.capacity = capacity;
+		this.fair = fair;
+		this.upperLimit = new Semaphore(this.capacity, this.fair);
+		this.lowerLimit = new Semaphore(0, this.fair);
 	}
 
 	@Override
 	public void put(int amount) throws InterruptedException {
-		// TODO
+		upperLimit.acquire(amount);
+		synchronized (this) {
+			currentAmount = currentAmount + amount;
+		}
+		lowerLimit.release(amount);
 	}
 
 	@Override
 	public void get(int amount) throws InterruptedException {
-		// TODO
+		lowerLimit.acquire(amount);
+		synchronized (this) {
+			currentAmount = currentAmount - amount;
+		}
+		upperLimit.release(amount);
 	}
 }
