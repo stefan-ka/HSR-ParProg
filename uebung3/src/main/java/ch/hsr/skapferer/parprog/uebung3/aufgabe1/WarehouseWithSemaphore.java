b3b19bb3d0c0350290c5e804baed69671a0de07c
Stefan Kapferer <stefan@kapferer.ch>
2015-03-04 13:40:14 +0100

    Uebung3/Aufgabe1: Added mutex in Semaphore Solution

--- uebung3/src/main/java/ch/hsr/skapferer/parprog/uebung3/aufgabe1/WarehouseWithSemaphore.java
+++ uebung3/src/main/java/ch/hsr/skapferer/parprog/uebung3/aufgabe1/WarehouseWithSemaphore.java
@@ -9,29 +9,31 @@ public class WarehouseWithSemaphore implements Warehouse {
 	private int currentAmount = 0;
 	private Semaphore upperLimit;
 	private Semaphore lowerLimit;
+	private Semaphore mutex;
 
 	public WarehouseWithSemaphore(int capacity, boolean fair) {
 		this.capacity = capacity;
 		this.fair = fair;
 		this.upperLimit = new Semaphore(this.capacity, this.fair);
 		this.lowerLimit = new Semaphore(0, this.fair);
+		this.mutex = new Semaphore(1, this.fair);
 	}
 
 	@Override
 	public void put(int amount) throws InterruptedException {
 		upperLimit.acquire(amount);
-		synchronized (this) {
-			currentAmount = currentAmount + amount;
-		}
+		mutex.acquire();
+		currentAmount = currentAmount + amount;
+		mutex.release();
 		lowerLimit.release(amount);
 	}
 
 	@Override
 	public void get(int amount) throws InterruptedException {
 		lowerLimit.acquire(amount);
-		synchronized (this) {
-			currentAmount = currentAmount - amount;
-		}
+		mutex.acquire();
+		currentAmount = currentAmount - amount;
+		mutex.release();
 		upperLimit.release(amount);
 	}
 }
