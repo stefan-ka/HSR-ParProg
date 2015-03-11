3b63d1b412d5aaee32c0c4d457216671f6041fa3
Stefan Kapferer <stefan@kapferer.ch>
2015-03-11 12:38:57 +0100

    Initialize Uebung4/Testat1

---
+++ uebung4-testat1/src/main/java/ch/hsr/skapferer/parprog/uebung4_testat1/aufgabe1/BrokenCyclicLatch.java
@@ -0,0 +1,38 @@
+package ch.hsr.skapferer.parprog.uebung4_testat1.aufgabe1;
+
+import java.util.concurrent.CountDownLatch;
+
+public class BrokenCyclicLatch {
+	private static final int NOF_ROUNDS = 10;
+	private static final int NOF_THREADS = 10;
+
+	private static CountDownLatch latch = new CountDownLatch(NOF_THREADS);
+
+	private static void multiRounds(int threadId) throws InterruptedException {
+		for (int round = 0; round < NOF_ROUNDS; round++) {
+			latch.countDown();
+			latch.await();
+			if (threadId == 0) {
+				latch = new CountDownLatch(NOF_THREADS); // new latch for new
+															// round
+			}
+			System.out.println("Round " + round + " thread " + threadId);
+		}
+	}
+
+	public static void main(String[] args) {
+		for (int i = 0; i < NOF_THREADS; i++) {
+			final int threadId = i;
+			new Thread() {
+				@Override
+				public void run() {
+					try {
+						multiRounds(threadId);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}.start();
+		}
+	}
+}
