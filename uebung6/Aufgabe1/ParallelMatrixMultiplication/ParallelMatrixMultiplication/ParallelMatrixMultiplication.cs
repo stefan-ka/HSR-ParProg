6b7296c0aa8f96e66d07010146a660b4285e7c1a
Stefan Kapferer <stefan@kapferer.ch>
2015-03-25 14:06:35 +0100

    Formatted Code in Aufgabe1

--- uebung6/Aufgabe1/ParallelMatrixMultiplication/ParallelMatrixMultiplication/ParallelMatrixMultiplication.cs
+++ uebung6/Aufgabe1/ParallelMatrixMultiplication/ParallelMatrixMultiplication/ParallelMatrixMultiplication.cs
@@ -2,27 +2,34 @@
 using System;
 using System.Threading.Tasks;
 
-namespace ParallelMatrixMultiplication {
-  public class ParallelMatrixMultiplication {
-    // Result = A * B, A has dimN x dimK, B has dimK x dimM, Result has dimN x dimM.
-    public static long[,] Multiply(long[,] matrixA, long[,] matrixB) {
-      var dimN = matrixA.GetLength(0);
-      var dimK = matrixA.GetLength(1);
-      if (dimK != matrixB.GetLength(0)) {
-        throw new ArgumentException("Dimensions for matrix multiplication do not match");
-      }
-      var dimM = matrixB.GetLength(1);
-      var matrixC = new long[dimN, dimM];
-      // Computation
-      for (int i = 0; i < dimN; i++) {
-        for (int j = 0; j < dimM; j++) {
-          matrixC[i, j] = 0;
-          for (int k = 0; k < dimK; k++) {
-            matrixC[i, j] += matrixA[i, k] * matrixB[k, j];
-          }
+namespace ParallelMatrixMultiplication
+{
+    public class ParallelMatrixMultiplication
+    {
+        // Result = A * B, A has dimN x dimK, B has dimK x dimM, Result has dimN x dimM.
+        public static long[,] Multiply(long[,] matrixA, long[,] matrixB)
+        {
+            var dimN = matrixA.GetLength(0);
+            var dimK = matrixA.GetLength(1);
+            if (dimK != matrixB.GetLength(0))
+            {
+                throw new ArgumentException("Dimensions for matrix multiplication do not match");
+            }
+            var dimM = matrixB.GetLength(1);
+            var matrixC = new long[dimN, dimM];
+            // Computation
+            for (int i = 0; i < dimN; i++)
+            {
+                for (int j = 0; j < dimM; j++)
+                {
+                    matrixC[i, j] = 0;
+                    for (int k = 0; k < dimK; k++)
+                    {
+                        matrixC[i, j] += matrixA[i, k] * matrixB[k, j];
+                    }
+                }
+            }
+            return matrixC;
         }
-      }
-      return matrixC;
     }
-  }
 }
--- uebung6/Aufgabe1/ParallelMatrixMultiplication/ParallelMatrixMultiplication/Program.cs
+++ uebung6/Aufgabe1/ParallelMatrixMultiplication/ParallelMatrixMultiplication/Program.cs
@@ -2,44 +2,55 @@
 using System;
 using System.Diagnostics;
 
-namespace ParallelMatrixMultiplication {
-  public class Program {
-    public static void Main() {
-      // A has dimension N x K, B has K x M, C has N x M
-      const int N = 1000;
-      const int M = 10000;
-      const int K = 100;
-      var random = new Random(4711);
-      var matrixA = _CreateRandomMatrix(random, N, K);
-      var matrixB = _CreateRandomMatrix(random, K, M);
-      var watch = Stopwatch.StartNew();
-      var matrixC = ParallelMatrixMultiplication.Multiply(matrixA, matrixB);
-      Console.WriteLine("Total computing time: {0} ms", watch.ElapsedMilliseconds);
-      _CheckCorrectness(matrixA, matrixB, matrixC);
-    }
-
-    private static long[,] _CreateRandomMatrix(Random random, long len1, long len2) {
-      var matrix = new long[len1, len2];
-      for (int i = 0; i < len1; i++) {
-        for (int j = 0; j < len2; j++) {
-          matrix[i, j] = random.Next() * random.Next();
+namespace ParallelMatrixMultiplication
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // A has dimension N x K, B has K x M, C has N x M
+            const int N = 1000;
+            const int M = 10000;
+            const int K = 100;
+            var random = new Random(4711);
+            var matrixA = _CreateRandomMatrix(random, N, K);
+            var matrixB = _CreateRandomMatrix(random, K, M);
+            var watch = Stopwatch.StartNew();
+            var matrixC = ParallelMatrixMultiplication.Multiply(matrixA, matrixB);
+            Console.WriteLine("Total computing time: {0} ms", watch.ElapsedMilliseconds);
+            _CheckCorrectness(matrixA, matrixB, matrixC);
         }
-      }
-      return matrix;
-    }
 
-    private static void _CheckCorrectness(long[,] matrixA, long[,] matrixB, long[,] matrixC) {
-      for (int i = 0; i < matrixC.GetLength(0); i++) {
-        for (int j = 0; j < matrixC.GetLength(1); j++) {
-          long sum = 0;
-          for (int k = 0; k < matrixA.GetLength(1); k++) {
-            sum += matrixA[i, k] * matrixB[k, j];
-          }
-          if (sum != matrixC[i, j]) {
-            throw new Exception("Incorrect matrix multiplication");
-          }
+        private static long[,] _CreateRandomMatrix(Random random, long len1, long len2)
+        {
+            var matrix = new long[len1, len2];
+            for (int i = 0; i < len1; i++)
+            {
+                for (int j = 0; j < len2; j++)
+                {
+                    matrix[i, j] = random.Next() * random.Next();
+                }
+            }
+            return matrix;
         }
-      }
+
+        private static void _CheckCorrectness(long[,] matrixA, long[,] matrixB, long[,] matrixC)
+        {
+            for (int i = 0; i < matrixC.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrixC.GetLength(1); j++)
+                {
+                    long sum = 0;
+                    for (int k = 0; k < matrixA.GetLength(1); k++)
+                    {
+                        sum += matrixA[i, k] * matrixB[k, j];
+                    }
+                    if (sum != matrixC[i, j])
+                    {
+                        throw new Exception("Incorrect matrix multiplication");
+                    }
+                }
+            }
+        }
     }
-  }
 }
