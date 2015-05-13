4d450a660951ce8f2d3a851834a40d3c444ad9ca
unknown <skapfere@DT-IT-116343-08.hsr.ch>
2015-05-13 13:52:28 +0200

    Uebung12/Aufgabe2: Implemented parallel Solution for MonteCarloPi and made measurements on HSR Cluster

--- uebung12-testat3/Aufgabe2/MonteCarloPi/MonteCarloPi.cs
+++ uebung12-testat3/Aufgabe2/MonteCarloPi/MonteCarloPi.cs
@@ -1,29 +1,48 @@
-﻿using System;
+﻿using MPI;
+using System;
 using System.Diagnostics;
 
-namespace MonteCarloPi {
-  public class MonteCarloPi {
-    private const long Trials = 100000000L;
+namespace MonteCarloPi
+{
+    public class MonteCarloPi
+    {
+        private const long Trials = 100000000L;
 
-    public static void Main(string[] args) {
-      var watch = Stopwatch.StartNew();
-      long hits = CountHits(Trials);
-      double pi = 4 * ((double)hits / Trials);
-      Console.WriteLine("Pi approximation {0} versus exact {1}", pi, Math.PI);
-      Console.WriteLine("Total time: {0} ms", watch.ElapsedMilliseconds);
-    }
+        public static void Main(string[] args)
+        {
+            using (new MPI.Environment(ref args))
+            {
+                var watch = Stopwatch.StartNew();
 
-    private static long CountHits(long trials) {
-      long hits = 0;
-      Random random = new Random();
-      for (long i = 0; i < trials; i++) {
-        double x = random.NextDouble();
-        double y = random.NextDouble();
-        if (x * x + y * y <= 1) { // identical to Math.Sqrt(x * x + y * y) <= 1
-          hits++;
+                int rank = Communicator.world.Rank;
+                long size = Communicator.world.Size;
+
+                long hits = CountHits(Trials / size);
+                long totalHits = Communicator.world.Allreduce(hits, (a, b) => a + b);
+
+                if (rank == 0)
+                {
+                    double pi = 4 * ((double)totalHits / Trials);
+                    Console.WriteLine("Pi approximation {0} versus exact {1}", pi, Math.PI);
+                    Console.WriteLine("Total time: {0} ms", watch.ElapsedMilliseconds);
+                }
+            }
         }
-      }
-      return hits;
+
+        private static long CountHits(long trials)
+        {
+            long hits = 0;
+            Random random = new Random();
+            for (long i = 0; i < trials; i++)
+            {
+                double x = random.NextDouble();
+                double y = random.NextDouble();
+                if (x * x + y * y <= 1)
+                { // identical to Math.Sqrt(x * x + y * y) <= 1
+                    hits++;
+                }
+            }
+            return hits;
+        }
     }
-  }
 }
